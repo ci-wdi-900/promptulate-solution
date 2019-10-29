aca6690dfcb6c40f2c15538df2ac81157b2a9001
Colin Jaffe <balloonasaurus@gmail.com>
2019-10-29 14:16:03 -0400

    Updates solution.

--- calc-you-later.js
+++ calc-you-later.js
@@ -1,16 +1,8 @@
 const userInput = prompt('Enter your mathematical question for promptulate to answer!').split(' ');
 
-const num1 = userInput[userInput.length - 2];
-const num2 = userInput[userInput.length - 1];
-
-// Take care of the fact that they might've passed in something like:
-// '3 added to 5'
-// Instead of:
-// '3 + 5';
-// If they did, we'll have an array of 4 items instead of 3!
-const operation = userInput.length > 3
-  ? `${userInput[0]} ${userInput[1]}`
-  : userInput[0];
+const operation = userInput[0];
+const num1 = userInput[1];
+const num2 = userInput[2];
 
 const result = calculate(operation, num1, num2);
 document.querySelector('#result').innerText = result;
